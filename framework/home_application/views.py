a976ef6140e248f090667e038958a4b2fdcf6caf
bagmdgiveme <bagmdgiveme@163.com>
2018-03-23 16:31:05 +0800

    添加multiplier

--- framework/home_application/views.py
+++ framework/home_application/views.py
@@ -11,17 +11,23 @@ See the License for the specific language governing permissions and limitations 
 
 from common.mymako import render_mako_context,render_json
 #from django.http import JsonResponse
-
+from home_application.models import MultRecord
 def home(request):
     """
     首页12
     """
+    all_record = MultRecord.object.all()
+    ctx = {
+        'all_record': all_record
+    }
     return render_mako_context(request, '/home_application/home.html')
 
 def multiplication_computer(request):
     multiplier = int(request.POST.get('multiplier'))
     multiplicand = int(request.POST.get('multiplicand'))
     mult_result = multiplier * multiplicand
+    mult_record = MultRecord(multiplier=multiplier,multiplicand=multiplicand,mult_result=mult_result)
+    mult_result.save()
     return render_json({'result': True,'mult_result': mult_result})
 
 def dev_guide(request):
